5fb2d970e80eb90979ee555377774b516039c404
Joseph <master@joeys-mbp.mynetworksettings.com>
2023-03-25 09:54:02 -0400

    1st push

--- server/server.js
+++ server/server.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userController = require('./userController');
 // const apiRouter = require('./routes/api.js');
 
 const PORT = 3000;
+/**
+ * handle parsing request body
+ */
+app.use(express.json());
 
+/**
+ * root
+ */
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
 
+//handle users signing up
+app.get('/signup', userController.createUser, (req, res) => {
+  //swap out json for redirect route
+  return res.status(200).json({});
+});
 
+//handle users logging up
+app.get('/login', userController.createUser, (req, res) => {
+  //swap out json for redirect route
+  return res.status(200).json({});
+});
 
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) =>
+  res.status(404).send("This is not the page you're looking for...")
+);
 
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
