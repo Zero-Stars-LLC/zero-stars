1ceba8292031206ca1e3daa0e4f3d4237d3ea2b3
Jacob Alarcon <jacobalarcon@gmail.com>
2023-03-25 10:26:30 -0500

    reformat skeleton of api

--- server/apiRequestHandler.js
+++ server/apiRequestHandler.js
@@ -3,7 +3,9 @@ const apiHandler = {};
 apiHandler.getData = async (req, res, next) => {
   try {
     // call google functino api handler will send along the place id
+    // places_id will be in req.body
     const data = { message: 'in api handler' };
+    // call to function with places_id and assign data to req.locals
     req.locals = data;
     next();
   } catch (error) {
